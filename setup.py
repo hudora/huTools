e5e9520b5bf9de41886995d46c7cb8d4bfceb797
md <md@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2007-06-07 15:05:01 +0000

    Setup fixed.
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/trunk/huTools@1956 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(name='huTools',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
       url='http://www.hosted-projects.com/trac/hudora/public/wiki', # /huTools',
-      version='0.1',
+      version='0.2',
       description='various tools and toys to make Python coding more fun',
       license='BSD',
       classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python'],
       
-        packages = find_packages(),
-        package_data = {
-            # If any package contains *.txt or *.rst files, include them:
-            '': ['*.txt', '*.rst'],
-        },
-      
-      zip_safe = True,
+      package_dir = {'huTools': ''},
+      packages = ['huTools', 'huTools.calendar'],
+      # This seems to be broken with certain Python versions:
+      #package_data = {
+      #      # If any package contains *.txt or *.rst files, include them:
+      #      '': ['*.txt', '*.rst'],
+      #},
 )
