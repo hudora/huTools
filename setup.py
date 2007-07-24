89f0543311a844c642f27b588f069d1cf65ef74f
md <md@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2007-07-24 07:13:14 +0000

    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/trunk/huTools@2177 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(name='huTools',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
       url='http://www.hudora.de/code/',
-      version='0.2.2',
+      version='0.2.1',
       description='Various tiny tools and toys to make Python coding less work more fun.',
       long_description='''huTools is a collection of many totally non earth shattering modules:
       Various tiny tools and toys to make Python coding less work more fun:
       
-      * calendar - date besed calculations and format conversions for HTTP and ATOM.
+      * calendar              - date based calculations and format conversions for HTTP and ATOM.
       * checksummming         - calculate various checksums including EAN/GTIN/NVE/SSCC and DPD GeoPost
       * luids                 - locally unique user-ids with strong guarantees of beeing unique.
       * printing              - access printers (very primitive)
@@ -20,6 +20,7 @@ setup(name='huTools',
       classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python'],
       
+      zip_safe=False, 
       package_dir = {'huTools': ''},
       packages = ['huTools', 'huTools.calendar'],
       # This seems to be broken with certain Python versions:
