6436d9631c75e9d0f2ebb8da4c796ccaeaa86396
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-09-01 03:41:49 +0000

    docfix
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huTools/trunk@3742 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- huTools/__init__.py
+++ huTools/__init__.py
@@ -1,13 +1,5 @@
 """This module collects several utility funictions we found useful at HUDORA. Noting special in there.
 Consider it BSD Licensed.
-
-calendar              - date besed calculations and format conversions
-checksummming         - calculate various checksums
-luids                 - locally unique user-ids
-printing              - access printers
-ReReadingConfigParser - ConfigParser which detects changes in config files
-unicode               - real-world unicode handling
-world                 - country codes
 """
 
 __revision__ = "$Revision$"
