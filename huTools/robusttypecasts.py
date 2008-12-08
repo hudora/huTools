8e83f944f17a1c20d351b0f4550e9a50f215d275
c.borgolte@hudora.de <c.borgolte@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-12-08 10:07:44 +0000

    Added float_or_0.
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huTools/trunk@4476 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- huTools/robusttypecasts.py
+++ huTools/robusttypecasts.py
@@ -32,6 +32,33 @@ def io0(data):
     except TypeError:
         return 0
 
+def float_or_0(data):
+    """Helper fnc. Returns data casted to a float value if possible, else to 0.
+    
+    >>> float_or_0(5.0)
+    5.0
+    >>> float_or_0(None)
+    0.0
+    >>> float_or_0(0)
+    0.0
+    >>> float_or_0(-1)
+    -1.0
+    >>> float_or_0([])
+    0.0
+    >>> float_or_0(tuple())
+    0.0
+    
+    """
+    if data == None:
+        return 0.0
+    try:
+        return float(data)
+    except (TypeError, ValueError):
+        # FIXME do we really want to allow to convert anything to zero here or is it better, not to
+        # catch the type and value errors an let casting fail?
+        return 0.0
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
