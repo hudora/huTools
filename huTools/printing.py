73ffdbdc901695856c1fde7a060c8d0690088e63
c.klein@hudora.de <c.klein@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2008-12-16 09:48:18 +0000

    Worked on huTools.printing and made print_data working again (It used to crash cups every time a Verladebericht was printed)
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huTools/trunk@4557 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- huTools/printing.py
+++ huTools/printing.py
@@ -6,32 +6,36 @@ printing.py simple minded toolkit for printing on CUPS.
 Created by Maximillian Dornseif on 2006-11-19. BSD Licensed.
 """
 
-import os
+import logging
+from subprocess import Popen, PIPE, call
 
 __revision__ = "$Revision$"
+
+LOG_FILENAME = '/tmp/huTools_lplog'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
 
 
 def print_file(filename, jobname=None, printer=None, copies=1):
     """Print a file."""
-    args = [] # '-n %d' % copies]
+    args = ['/usr/local/bin/lpr', '-#%d' % copies]
     if printer:
-        args.append('-P %r' % str(printer))
-    # we create several separate print job
-    for i in range(copies):
-        os.system('/usr/local/bin/lpr %s %r' % (' '.join(args), filename))
-    
+        args.append('-P%s' % str(printer))
+    args.append('"%s"' % filename)
+    logging.info(' '.join(args))
+    call(args)
 
 def print_data(data, jobname=None, printer=None, copies=1, printserver='printserver.local.hudora.biz'):
-    """Print a file."""
-    args = [] # ['-n %d' % copies]
+    """Print a datastream."""
+    args = ['/usr/local/bin/lpr', '-#%d' % copies]
     if printer:
-        args.append('-P %r' % str(printer))
+        args.append('-P%s' % str(printer))
+
     #if printserver:
     #    args.append('-H %r' % str(printserver))
     #if jobname:
     #    args.append('-J %r' % jobname.replace("'\";./ ", "_"))
-    os.system('echo /usr/local/bin/lpr %s >> /tmp/huTools_lplog' % (' '.join(args), ))
-    for i in range(copies):
-        fd = os.popen('/usr/local/bin/lpr %s >> /tmp/huTools_lplog' % (' '.join(args), ), 'w')
-        fd.write(data)
-        fd.close()
+
+    logging.info(' '.join(args))
+    pipe = Popen(args, shell=False, stdin=PIPE).stdin
+    pipe.write(data)
+    pipe.close()
