44bc1c1e9a39c28694b6e66ec5851b84e8c86499
Christian Klein <c.klein@hudora.de>
2010-03-26 10:08:41 +0100

    ajax_request entfernt (wird nach huDjango verschoben)

--- huTools/decorators.py
+++ huTools/decorators.py
@@ -12,9 +12,8 @@ import cPickle as pickle
 import functools
 import hashlib
 from decorator import decorator
-from django.http import HttpResponse
 from functools import wraps
-import simplejson as json
+
 
 def _getattr_(obj, name, default_thunk):
     "Similar to .setdefault in dictionaries."
@@ -116,35 +115,3 @@ def none_on_exception(func):
             return None
     return _decorator
 
-
-# The ajax_request decorator was taken from django-annoying
-# see http://pypi.python.org/pypi/django-annoying/0.7.4
-
-class JsonResponse(HttpResponse):
-    """
-    HttpResponse descendant, which return response with ``application/json`` mimetype.
-    """
-    def __init__(self, data):
-        super(JsonResponse, self).__init__(content=json.dumps(data), mimetype='application/json')
-
-
-def ajax_request(func):
-    """
-    If view returned serializable dict, returns JsonResponse with this dict as content.
-
-    example:
-
-        @ajax_request
-        def my_view(request):
-            news = News.objects.all()
-            news_titles = [entry.title for entry in news]
-            return {'news_titles': news_titles}
-    """
-    @wraps(func)
-    def wrapper(request, *args, **kwargs):
-        response = func(request, *args, **kwargs)
-        if isinstance(response, dict):
-            return JsonResponse(response)
-        else:
-            return response
-    return wrapper
