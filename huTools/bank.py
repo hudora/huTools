1dbcb60253704db9437684f7c5144a2fe0f0a43d
Christian Klein <c.klein@hudora.de>
2010-03-26 10:09:19 +0100

    Code zur Berechnung und Validierung von IBAN

--- huTools/bank.py
+++ huTools/bank.py
@@ -7,15 +7,16 @@ Created by Christian Klein on 2010-03-25.
 Copyright (c) 2010 Christian Klein. All rights reserved.
 """
 
+from string import ascii_uppercase
 
 def convert_character(string):
-    """Konvertierung von nicht-numerischen Zeichen in IBAN"""
+    """Konvertierung von nicht-numerischen Zeichen in einer IBAN"""
     result = []
     for character in string:
         if character.isdigit():
             result.append(character)
         else:
-            result.append(str("ABCDEFGHIJKLMNOPQRSTUVWXYZ".index(character) + 10))
+            result.append(str(ascii_uppercase.index(character) + 10))
 
     return "".join(result)
 
@@ -25,7 +26,10 @@ def iban(ktonr, blz, land="DE"):
     Berechnet die International Bank Account Number für deutsche Konten
     
     Zunächst wird die Basic Bank Account Number für deutsche Konten berechnet.
-    Aus diesem, XXX
+    Der Laendercode wird mit zwei '0' aufgefüllt und an die BBAN gehangen.
+    Aus diesem wird der Prüfcode als Modulo 97-10 (DIN ISO 7064) berechnet.
+
+    >>> 
     """
     
     bban = "%08d%010d" % (int(blz), int(ktonr))
@@ -35,10 +39,5 @@ def iban(ktonr, blz, land="DE"):
 
 
 def check_iban(value):
+    """Validiere (errechnete) IBAN"""
     return int(convert_character(value[4:] + value[:4])) % 97 == 1
-
-
-if __name__ == "__main__":
-    x = iban("1234567890", "70090100")
-    print x
-    print check_iban(x)
