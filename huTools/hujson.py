051ca9612b5d75a0c222ea7b69f157af086023eb
Maximillian Dornseif <md@hudora.de>
2010-09-26 22:05:17 +0200

    added hujson.loads

--- huTools/hujson.py
+++ huTools/hujson.py
@@ -4,7 +4,8 @@
 hujson.py - extended json - tries to be compatible with simplejson
 
 hujson can encode additional types like decimal and datetime into valid json.
-All the heavy lifting is done by John Millikin's `jsonlib`.
+All the heavy lifting is done by John Millikin's `jsonlib`, see
+https://launchpad.net/jsonlib
 
 Created by Maximillian Dornseif on 2010-09-10.
 Copyright (c) 2010 HUDORA. All rights reserved.
@@ -19,10 +20,14 @@ import datetime
 def _unknown_handler(value):
     if isinstance(value, datetime.date):
         return str(value)
-    elif isinstance(obj, datetime.datetime):
+    elif isinstance(value, datetime.datetime):
         return value.isoformat() + 'Z'
-    raise UnknownSerializerError
+    raise UnknownSerializerError("%s(%s)" % (type(value), value))
 
 
 def dumps(val):
-    return _jsonlib.write(val, on_unknown=_unknown_handler)
+    return _jsonlib.write(val, on_unknown=_unknown_handler, indent=' ')
+
+
+def loads(data):
+    return _jsonlib.read(data)
