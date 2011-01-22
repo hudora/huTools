15c624363d42b55f873de41f6827889275e1d16d
Maximillian Dornseif <md@hudora.de>
2011-01-22 01:01:07 +0100

    0.61:   huTools.structured.Struct and make_struct handle lists and get an items() method

--- huTools/structured.py
+++ huTools/structured.py
@@ -11,43 +11,133 @@ structured.py - handle structured data/dicts/objects
 
 import xml.etree.cElementTree as ET
 
-
-# siehe http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
+# Basic conversation goal here is converting a dict to an object allowing
+# more comfortable access. `Struct()` and `make_struct()` are used to archive
+# this goal.
+# See http://stackoverflow.com/questions/1305532/convert-python-dict-to-object for the inital Idea
+# 
+# The reasoning for this is the observation that we ferry arround hundreds of dicts via JSON
+# and accessing them as `obj['key']` is tiresome after some time. `obj.key` is much nicer.
 class Struct(object):
+    """Emulate a cross over between a dict() and an object()."""
     def __init__(self, entries, default=None, nodefault=False):
         # ensure all keys are strings and nothing else
         entries = dict([(str(x), y) for x, y in entries.items()])
         self.__dict__.update(entries)
-        self.default = default
-        self.nodefault = nodefault
+        self.__default = default
+        self.__nodefault = nodefault
 
     def __getattr__(self, name):
-        if self.nodefault:
+        """Emulate Object access.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.a
+        'b'
+        >>> obj.foobar
+        'c'
+
+        `hasattr` results in strange behaviour if you give a default value. This might change in the future.
+        >>> hasattr(obj, 'a')
+        True
+        >>> hasattr(obj, 'foobar')
+        True
+        """
+        if self.__nodefault:
             raise AttributeError("'<Struct>' object has no attribute '%s'" % name)
         if name.startswith('_'):
             # copy expects __deepcopy__, __getnewargs__ to raise AttributeError
             # see http://groups.google.com/group/comp.lang.python/browse_thread/thread/6ac8a11de4e2526f/
             # e76b9fbb1b2ee171?#e76b9fbb1b2ee171
             raise AttributeError("'<Struct>' object has no attribute '%s'" % name)
-        return self.default
+        return self.__default
 
     def __getitem__(self, key):
+        """Emulate dict like access.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj['a']
+        'b'
+
+        While the standard dict access via [key] uses the default given when creating the struct,
+        access via get(), results in None for keys not set. This might be considered a bug and
+        should change in the future.
+        >>> obj['foobar']
+        'c'
+        >>> obj.get('foobar')
+        'c'
+        """
         # warnings.warn("dict_accss[foo] on a Struct, use object_access.foo instead",
         #                DeprecationWarning, stacklevel=2)
-        if self.nodefault:
+        if self.__nodefault:
             return self.__dict__[key]
-        return self.__dict__.get(key, self.default)
+        return self.__dict__.get(key, self.__default)
 
     def get(self, key, default=None):
+        """Emulate dictionary access.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.get('a')
+        'b'
+        >>> obj.get('foobar')
+        'c'
+        """
         if key in self.__dict__:
             return self.__dict__[key]
+        if not self.__nodefault:
+            return self.__default
         return default
 
     def __contains__(self, item):
+        """Emulate dict 'in' functionality.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> 'a' in obj
+        True
+        >>> 'foobar' in obj
+        False
+        """
         return item in self.__dict__
 
+    def has_key(self, item):
+        """Emulate dict.has_key() functionality.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.has_key('a')
+        True
+        >>> obj.has_key('foobar')
+        False
+        """
+        return item in self
+
+    def items(self):
+        """Emulate dict.items() functionality.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.items()
+        [('a', 'b')]
+        """
+        return [(k, v) for (k, v) in self.__dict__.items() if not k.startswith('_Struct__')]
+
+    def keys(self):
+        """Emulate dict.keys() functionality.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.keys()
+        ['a']
+        """
+        return [k for (k, v) in self.__dict__.items() if not k.startswith('_Struct__')]
+
+    def values(self):
+        """Emulate dict.values() functionality.
+
+        >>> obj = Struct({'a': 'b'}, default='c')
+        >>> obj.values()
+        ['b']
+        """
+        return [v for (k, v) in self.__dict__.items() if not k.startswith('_Struct__')]
+
     def __repr__(self):
-        return "<Struct: %r>" % self.__dict__
+        return "<Struct: %r>" % dict(self.items())
 
 
 def make_struct(obj, default=None, nodefault=False):
@@ -59,7 +149,7 @@ def make_struct(obj, default=None, nodefault=False):
     >>> obj.foo
     'bar'
 
-    make_struct leaves objects alone.
+    `make_struct` leaves objects alone.
     >>> class MyObj(object): pass
     >>> data = MyObj()
     >>> data.foo = 'bar'
@@ -67,11 +157,31 @@ def make_struct(obj, default=None, nodefault=False):
     >>> obj.foo
     'bar'
 
-    make_struct also is idempotent
+    `make_struct` also is idempotent
     >>> obj = make_struct(make_struct(dict(foo='bar')))
     >>> obj.foo
     'bar'
 
+    `make_struct` recursively handles dicts and lists of dicts
+    >>> obj = make_struct(dict(foo=dict(bar='baz')))
+    >>> obj.foo.bar
+    'baz'
+
+    >>> obj = make_struct([dict(foo='baz')])
+    >>> obj
+    [<Struct: {'foo': 'baz'}>]
+    >>> obj[0].foo
+    'baz'
+
+    >>> obj = make_struct(dict(foo=dict(bar=dict(baz='end'))))
+    >>> obj.foo.bar.baz
+    'end'
+
+    >>> obj = make_struct(dict(foo=[dict(bar='baz')]))
+    >>> obj.foo[0].bar
+    'baz'
+    >>> obj.items()
+    [('foo', [<Struct: {'bar': 'baz'}>])]
     """
     if (not hasattr(obj, '__dict__')) and hasattr(obj, 'iterkeys'):
         # this should be a dict
@@ -80,6 +190,9 @@ def make_struct(obj, default=None, nodefault=False):
         for k, v in obj.items():
             setattr(struc, k, make_struct(v, default, nodefault))
         return struc
+    elif hasattr(obj, '__delslice__') and hasattr(obj, '__getitem__'):
+        #
+        return [make_struct(v, default, nodefault) for v in obj]
     else:
         return obj
 
@@ -302,7 +415,8 @@ def test():
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
+    import sys
+    failure_count, test_count = doctest.testmod()
     d = make_struct({
         'item1': 'string',
         'item2': ['dies', 'ist', 'eine', 'liste'],
@@ -319,5 +433,5 @@ if __name__ == '__main__':
     d.item3.dict
     d.item4
     d.item5
-
     test()
+    sys.exit(failure_count)
