1a41c5449377d74e5e9aff85b6ff07fb36495f50
Philipp Benjamin Köppchen <b.koeppchen@hudora.de>
2011-07-18 10:21:58 +0200

    [LH#1070] `Accept-Encoding: gzip` in http wieder eingebaut
    Das anfordern von gezipptem Content war nach [LH#1003] deaktiviert, da er nicht korrekt dekodiert wurde.
    Tests (siehe [LH#1003] und [LH#1070], hier ein ausführlicheres Experiemnt) haben ergeben, dass das Problem
    nicht mehr besteht, und deshalb der Header wieder gesendet werden kann.
    
    Der Header wurde ausserdem von 'Accept-Encoding: gzip;q=1.0, *;q=0' zu 'Accept-Encoding: gzip' geändert,
    da '*;q=0' laut http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.9 bedeutet: 'Alles andere ist nicht
    akzeptabel', und laut http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3 der Webserver, wenn er
    gzip nicht liefern kann, als Antwort '406 Not Acceptable' liefern SOLLTE. Das implizite Encoding 'Identity'
    wird so explizit ausgenommen.
    
    Ein 'Accept-Encoding: gzip' entspricht der Vorgehensweise der Browser, wie man z.B. mit
    http://www.delorie.com:81/some/url.html überprüfen kann.
    
    Change-Id: Ia8f60dac38e09ac6516c7a81113dc8b750e8b4a4
    Reviewed-on: http://gerrit.hudora.biz/741
    Tested-by: HudsonAutomation <edv+hudson@hudora.de>
    Reviewed-by: Christoph Borgolte <c.borgolte@hudora.de>
    Reviewed-by: Axel Schlueter <axel@pqrs.de>

--- huTools/http/__init__.py
+++ huTools/http/__init__.py
@@ -65,10 +65,7 @@ def fetch(url, content='', method='GET', credentials=None, headers=None, multipa
        enforced.
     """
 
-    myheaders = {
-                 # Für [LH#1003] als Hotfix erstmal abgeschaltet
-                 # urlfetch.fetch erkennt den Response Header "content-encoding: gzip" scheinbar nicht
-                 #'Accept-Encoding': 'gzip;q=1.0, *;q=0',
+    myheaders = {'Accept-Encoding': 'gzip',
                  'User-Agent': '%s/huTools.http (gzip)' % ua}
     if headers:
         myheaders.update(headers)
